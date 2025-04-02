7c30044d7ad5a7bf52630a23cefc0a2575942f19
kondo12340 <bartosz160420031604@gmail.com>
2025-04-02 14:30:10 +0000

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+       
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+      
+        for (int i = 10; i >= 1; i--) {
+      
+            for (int j = 0; j < 10 - i; j++) {
+                System.out.print(" ");
+            }
+           
+            for (int j = 0; j < (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+ 
+        }
+    }
 }
